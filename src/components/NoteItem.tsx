0c4553e532db554017385c76b93a2fa528b207e0
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-05-15 20:40:53 +0700

    feat: make the note layout responsive
    The delete and archive button will move to the bottom and there's
    "Delete" and "Archive" text on mobile

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -18,28 +18,28 @@ export default function NoteItem({
           item.active ? "bg-blue-400" : "bg-gray-300",
         )}
       />
-      <div className="p-5 space-y-4 text-gray-800">
-        <div className="flex">
-          <div className="w-full">
-            <p className="font-medium text-xl">{item.title}</p>
-            <p className="text-sm text-gray-500">~{item.date}</p>
-          </div>
-          <div className="flex gap-2 shrink-0">
-            <button
-              className="flex justify-center items-center rounded-lg border border-red-600 bg-red-600/20 text-red-600 size-10 cursor-pointer"
-              onClick={() => del(item)}
-            >
-              <Trash2 size={18} />
-            </button>
-            <button
-              className="flex justify-center items-center rounded-lg border border-orange-600 bg-orange-600/20 text-orange-600 size-10 cursor-pointer"
-              onClick={() => archive(item)}
-            >
-              <Archive size={18} />
-            </button>
-          </div>
+      <div className="p-5 space-y-4 text-gray-800 grid sm:grid-cols-[1fr_auto] grid-rows[repeat(3,auto)] sm:grid-rows-[repeat(2,auto)]">
+        <div className="w-full">
+          <p className="font-medium text-xl">{item.title}</p>
+          <p className="text-sm text-gray-500">~{item.date}</p>
         </div>
-        <p className="min-h-12">{item.desc}</p>
+        <div className="flex gap-2 sm:w-fit row-start-3 sm:row-start-1 sm:col-start-2">
+          <button
+            className="flex justify-center items-center gap-2 rounded-lg border border-red-600 bg-red-600/20 text-red-600 w-full h-10 sm:size-10 cursor-pointer"
+            onClick={() => del(item)}
+          >
+            <Trash2 size={18} />
+            <p className="sm:hidden">Delete</p>
+          </button>
+          <button
+            className="flex justify-center items-center gap-2 rounded-lg border border-orange-600 bg-orange-600/20 text-orange-600 w-full h-10 sm:size-10 cursor-pointer"
+            onClick={() => archive(item)}
+          >
+            <Archive size={18} />
+            <p className="sm:hidden">Archive</p>
+          </button>
+        </div>
+        <p className="min-h-12 sm:col-span-2">{item.desc}</p>
       </div>
     </div>
   );
