b8984c398cc3abf3a887d7a9336e9a4dbe9c9b39
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-05-15 20:29:23 +0700

    feat: make the navbar responsive and collapsible search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,67 @@
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar({
   setSearch,
 }: {
   setSearch: React.Dispatch<string>;
 }) {
+  const [searchOpen, setsearchOpen] = useState(false);
+  useEffect(() => {
+    console.log(searchOpen);
+  }, [searchOpen]);
+
+  const input = useRef<HTMLInputElement>(null);
+
+  function focusInput() {
+    input.current?.focus();
+  }
+
   return (
-    <nav className="fixed top-0 inset-x-0 container mx-auto py-7 flex justify-between">
-      <img src="/img/logo.svg" alt="Logo" />
-      <label className="inline-flex gap-2 text-gray-400 bg-gray-100 outline outline-gray-200 px-2.5 py-2 rounded-lg">
+    <nav
+      className={twMerge(
+        "fixed top-0 inset-x-0 container mx-auto px-5 py-7 flex gap-5 bg-white/50 backdrop-blur-xl",
+        searchOpen ? "justify-end lg:justify-between" : "justify-between",
+      )}
+    >
+      <img
+        src="/img/logo.svg"
+        alt="Logo"
+        className={searchOpen ? "hidden lg:inline" : ""}
+      />
+      <label
+        className={twMerge(
+          "cursor-pointer flex gap-2 text-gray-400 bg-gray-100 outline px-2.5 h-10 outline-gray-200 justify-center items-center rounded-lg",
+          !searchOpen && "size-10 lg:h-fill lg:w-fit",
+        )}
+        htmlFor="search"
+        onClick={() => {
+          if (!searchOpen) setsearchOpen(!searchOpen);
+          setTimeout(() => {
+            focusInput();
+          }, 0);
+        }}
+      >
         <Search />
         <input
           type="text"
           placeholder="Search..."
-          className="outline-none"
+          name="search"
+          className={twMerge("outline-none", !searchOpen && "hidden lg:inline")}
+          ref={input}
           onChange={(e) => setSearch(e.target.value)}
+          autoFocus
         />
       </label>
+      {searchOpen && (
+        <button
+          className="bg-gray-100 outline outline-gray-200 size-10 inline-flex justify-center items-center rounded-lg text-gray-400 cursor-pointer lg:hidden"
+          onClick={() => setsearchOpen(!searchOpen)}
+        >
+          <X />
+        </button>
+      )}
     </nav>
   );
 }
